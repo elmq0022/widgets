dfbd76f97560244f1fc73d1e2ac9c93e204b9257
LeakedMemory <elmq0022@umn.edu>
2020-12-26 17:03:50 -0600

    gets through useState hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Accordion from "./components/Accordion";
 
-function App() {
+export default () => {
+  const items = [
+    {
+      title: "What it react",
+      content: "React is a front end js framework"
+    },
+    {
+      title: "Why use react",
+      content: "Lots of people use it"
+    },
+    {
+      title: "How do you use react",
+      content: "Make components that react"
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <br />
+      <Accordion items={items}/>
     </div>
   );
-}
-
-export default App;
+};
